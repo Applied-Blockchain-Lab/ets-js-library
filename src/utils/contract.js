a4224247d5b3de73174bd5e95276d1cedb36ded2
drseu55 <alpics1337@gmail.com>
2022-11-18 12:56:26 +0200

    feat: Add ticket marketplace facet abi

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -6,11 +6,13 @@ import {
   EVENT_FACET_ABI,
   TICKET_CONTROLLER_FACET_ABI,
   TICKET_FACET_ABI,
+  TICKET_MARKETPLACE_FACET_ABI,
 } from "#config";
 
 const provider = ethers.getDefaultProvider(NET_RPC_URL);
 const eventsContract = new ethers.Contract(EVENTS_CONTRACT_ADDRESS, EVENT_FACET_ABI, provider);
 const ticketControllerContract = new ethers.Contract(EVENTS_CONTRACT_ADDRESS, TICKET_CONTROLLER_FACET_ABI, provider);
 const ticketsContract = new ethers.Contract(TICKETS_CONTRACT_ADDRESS, TICKET_FACET_ABI, provider);
+const ticketMarketplaceContract = new ethers.Contract(TICKETS_CONTRACT_ADDRESS, TICKET_MARKETPLACE_FACET_ABI, provider);
 
-export { eventsContract, ticketControllerContract, ticketsContract };
+export { eventsContract, ticketControllerContract, ticketsContract, ticketMarketplaceContract };
